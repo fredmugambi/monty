8599295175487c8972610be8f2db4e749586735b
fredmugambi <fredmugambi7@gmail.com>
2023-08-30 02:25:27 -0700

    monty

---
+++ nop.c
@@ -0,0 +1,13 @@
+Can't create, edit, or upload … You don't have enough storage to create, edit, and upload files. Get 100 GB of storage for Ksh 200.00 Ksh 50.00/month for 3 months.
+#include "monty.h"
+/**
+  *f_nop- nothing
+  *@head: stack head
+  *@counter: line_number
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
